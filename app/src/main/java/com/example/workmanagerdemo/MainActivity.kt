9e5c4ba44cae4eb63558afc0d69681be239383a0
mobcoder-nikhil-singh <nikhil.vadhva@mobcoder.com>
2020-11-25 14:56:48 +0530

    OneTimeWorkRequest Created

--- app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.workmanagerdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkRequest
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    var mWorkRequest: WorkRequest =
+        OneTimeWorkRequest.Builder(NotificationWorker::class.java)
+            .build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btStart.setOnClickListener {
+            WorkManager.getInstance(this).enqueue(mWorkRequest)
+        }
     }
 }
