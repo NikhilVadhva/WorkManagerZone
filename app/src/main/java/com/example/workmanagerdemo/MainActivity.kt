5cf241589cb7e7b99eb072a9ace987ca93a56035
mobcoder-nikhil-singh <nikhil.vadhva@mobcoder.com>
2020-11-26 11:49:35 +0530

    Enqueue work and observe the workInfo states

--- app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.workmanagerdemo
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.work.OneTimeWorkRequest
-import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import androidx.work.WorkRequest
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
 
     var mWorkRequest: WorkRequest =
         OneTimeWorkRequest.Builder(NotificationWorker::class.java)
@@ -22,5 +25,28 @@ class MainActivity : AppCompatActivity() {
         btStart.setOnClickListener {
             WorkManager.getInstance(this).enqueue(mWorkRequest)
         }
+
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(mWorkRequest.id)
+            .observe(this, Observer{ workInfo ->
+                when (workInfo?.state) {
+                    WorkInfo.State.SUCCEEDED -> {
+                        Toast.makeText(this,"Success Notification Task",Toast.LENGTH_LONG).show()
+                    }
+                    WorkInfo.State.FAILED -> {
+                        Toast.makeText(this,"Failed Notification Task",Toast.LENGTH_LONG).show()
+                    }
+                    WorkInfo.State.CANCELLED -> {
+                        Toast.makeText(this,"Cancelled Notification Task",Toast.LENGTH_LONG).show()
+                    }
+                    else ->
+                        Toast.makeText(this,"Running Notification Task",Toast.LENGTH_LONG).show()
+                }
+
+                if(workInfo.state.isFinished)
+                    Toast.makeText(this,"Notification Task Finished",Toast.LENGTH_LONG).show()
+            })
+
+
+
     }
 }
